e754ed714363020ad21b5540caa824a60d01d2a4
userbhoneron <bhoneron@gmail.com>
2026-04-11 18:42:21 +0630

    HTML structure and JS code

--- netlify/functions/rates.js
+++ netlify/functions/rates.js
@@ -1,8 +1,26 @@
-let rates = {};
+exports.handler = async function () {
+    try {
+        const res = await fetch("https://api.currencyfreaks.com/latest?apikey=4058d2d572f04303bcaec84ac205ea35");
 
-async function loadRates() {
-    const res = await fetch("/.netlify/functions/rates");
-    const data = await res.json();
+        const data = await res.json();
 
-    rates = data.rates;
-}
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                rates: {
+                    USD: 1,
+                    THB: parseFloat(data.rates.THB),
+                    MMK: parseFloat(data.rates.MMK)
+                }
+            })
+        };
+
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                error: "API failed"
+            })
+        };
+    }
+};
